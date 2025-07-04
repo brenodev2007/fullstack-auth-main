23914bf7078477b15031a1332ffce2009a93b4c0
Breno Soriani <breno.soriani@gmail.com>
2025-07-04 16:48:57 -0300

    Extraindo o Token

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,7 +1,27 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { authConfig } from "@/AuthConfig/auth";
+import { sign } from "jsonwebtoken";
+const fakeUser = {
+  id: 1,
+  username: "rodrigo",
+  password: "123456",
+};
 
 class SessionsController {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    const { username, password } = request.body;
+    if (username !== fakeUser.username && password !== fakeUser.password) {
+      throw new AppError("Invalid credentials");
+    }
+
+    const { secret } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: fakeUser.username,
+    });
+
+    return response.status(200).json({ token }); //retorna o token do usuário
+  }
 }
 
-export { SessionsController }
+export { SessionsController };
